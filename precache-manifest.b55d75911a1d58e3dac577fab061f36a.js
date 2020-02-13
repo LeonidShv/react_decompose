d4a0f33a5af02b1d509aa19e1d94f1e418eb5fc4
Leonid <matembit1996@gmail.com>
2020-02-13 15:05:21 +0200

    Updates

---
+++ precache-manifest.b55d75911a1d58e3dac577fab061f36a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9876f9bc3ee03e25fa1b1ecc9885e137",
+    "url": "./index.html"
+  },
+  {
+    "revision": "896aae36684c23f66005",
+    "url": "./static/css/main.d5da5087.chunk.css"
+  },
+  {
+    "revision": "cc4f4bd7f33883810bbb",
+    "url": "./static/js/2.46faed67.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.46faed67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "896aae36684c23f66005",
+    "url": "./static/js/main.18c45a22.chunk.js"
+  },
+  {
+    "revision": "e67b79006193623cc7c8",
+    "url": "./static/js/runtime-main.a19c704a.js"
+  }
+]);
